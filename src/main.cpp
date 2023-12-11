97d520d5989934a60c7a93a1508902b8f65a76bb
Marek S. Lukasiewicz <marek@lukasiewicz.tech>
2023-12-11 12:32:02 +0100

    Add msgpack with conan

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 
+#include "msgpack.hpp"
 #include "zmqpp/zmqpp.hpp"
 
 int main(int argc, char *argv[]) {
+    std::cout << "msgpack-cxx version: " << msgpack_version() << std::endl;
     std::cout << "zmqpp version: " << zmqpp::version() << std::endl;
     return EXIT_SUCCESS;
 }
